ec99527b0f9d3e24633f7a7b4ca75c4d050ca453
Thorsten Kurth <thorstenkurth@me.com>
2015-04-29 22:07:05 -0700

    dbg

--- arXivHarvest.py
+++ arXivHarvest.py
@@ -2,14 +2,14 @@ from sickle import Sickle
 import cElementTree as ET
 
 class arXivHarvest:
-    sickle=Null
+    sickle
     baseURL='http://export.arxiv.org/oai2'
 
     def __init__(self,setname='physics:hep-lat'):
-        sickle=Sickle(baseURL)
+        self.sickle=Sickle(baseURL)
 
     def GetRecords(self,setname='physics:hep-lat'):
-        return sickle.ListRecords(metadataPrefix='oai_dc',set=setname)
+        return self.sickle.ListRecords(metadataPrefix='oai_dc',set=setname)
 
     def GetValue(self,record,tag):
         dom = ET.parse(open(record, "r"))
