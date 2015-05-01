1f2d02c95ec14efb8a3a9da440ee6b6a31318c03
Thorsten Kurth <thorstenkurth@me.com>
2015-05-01 16:45:49 -0700

    dbg

--- LuscherClm.cpp
+++ LuscherClm.cpp
@@ -8,6 +8,21 @@
 
 #include "LuscherClm.hpp"
 
+/** to-python convert to complex<double> */
+struct dcomplex_to_python_object
+{
+    static PyObject* convert(dcomplex const& comp)
+    {
+        if(fabs(comp.im())<std::numeric_limits<double>::epsilon()){
+            boost::python::object result=boost::python::object(complex<double>(comp.re(),comp.im()));
+            return boost::python::incref(result.ptr());
+        }
+        else{
+            return Py_BuildValue("d",comp.re());
+        }
+    }
+};
+
 
 BOOST_PYTHON_MODULE(threevecd)
 {
@@ -17,6 +32,6 @@ BOOST_PYTHON_MODULE(threevecd)
 BOOST_PYTHON_MODULE(LuscherClm)
 {
     class_<Zetafunc>("LuscherClm",init<int,int, optional<double,threevec<double>,double,int> >())
-    .def("eval",&Zetafunc::operator());
+    .def("eval",&Zetafunc::operator(),return_value_policy<return_by_value>());
     boost::python::to_python_converter<dcomplex,dcomplex_to_python_object>();
 }
